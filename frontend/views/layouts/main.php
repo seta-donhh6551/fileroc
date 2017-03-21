841743d768fdc3c8efbe377f0604c6f06e5faf4d
caominhtung1992 <caominhtung64@gmail.com>
2017-03-21 23:17:56 +0700

    update js

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -43,6 +43,7 @@ AppAsset::register($this);
 
         <!-- jQuery -->
         <script type="text/javascript" src="<?= Yii::$app->request->baseUrl; ?>js/px.js"></script>
+        <script>(function(w,d,u){w.readyQ=[];w.bindReadyQ=[];function p(x,y){if(x=="ready"){w.bindReadyQ.push(y);}else{w.readyQ.push(x);}};var a={ready:p,bind:p};w.$=w.jQuery=function(f){if(f===d||f===u){return a}else{p(f)}}})(window,document)</script>
     </head>
     
     <body class="en">
